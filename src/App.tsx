3072d0621bd85aa396586c18829b945a376ea567
itprogaur <professionalcodehub@gmail.com>
2022-01-09 15:07:14 +0530

    Header Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './Frontend/header';
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+class App extends React.Component{
+
+  constructor(
+    super();
+  )
+
 }
 
 export default App;
---
+++ src/Frontend/header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+//import './public/cover.jpg'
+
+
+function Header(){
+    <div className='container'>
+        <div className='container'>
+            <h1>DARK Fashion ZONE</h1>
+            <img src='{cover.jpg}' alt='Header-cover-image' />
+            
+        </div>
+    </div>
+}
+
+export default Header;
